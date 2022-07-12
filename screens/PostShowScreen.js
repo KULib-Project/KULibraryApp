85306c751aea3dc69571a1c5a120dc5032da675b
wakhoo <qqww212@korea.ac.kr>
2022-07-12 21:09:04 +0900

    fin postlist >> after comment end add details

--- screens/PostShowScreen.js
+++ screens/PostShowScreen.js
@@ -6,7 +6,8 @@ import {
   ActivityIndicator,
   TouchableOpacity,
   View,
-  Image
+  Image,
+  Dimensions
 } from 'react-native';
 import {
   ImageHeaderScrollView,
@@ -15,8 +16,10 @@ import {
 import axios from 'axios';
 import async from "async";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { FullWindowOverlay } from "react-native-screens";
 
-
+const chartHeight = Dimensions.get('window').height;
+const chartWidth = Dimensions.get('window').width;
 const PostList =({navigation})=>{
 
     const [data,setData]=useState([]);
@@ -39,20 +42,20 @@ const PostList =({navigation})=>{
         return (
           //아이템 리스트
           <TouchableOpacity
-            ///onPress={() => navigation.replace("Detail", { itemData: item })}
+            ///onPress={() => navigation.navigate("Detail", { itemData: item })}
             onPress={() => {
               console.log(item);
               navigation.navigate("Detail", { itemData: item });
             }}
           >
             <View style={styles.rowWrapper}>
-              <View style={styles.cardImgWrapper}>
-              <Text style={styles.cardTitle}>{`${item.given_name}`}</Text>
-              </View>
               <View style={styles.cardInfo}>
                 <Text style={styles.cardTitle}>{`${item.title}`}</Text>
-                <Text style={styles.fontSize}>{`${item.content}`}</Text>
+                <Text style={styles.contentFont}>{`${item.content}`}</Text>
+                <View style={styles.rowWrapper}>
+                <Text style={styles.fontSize}>{`작성자 ${item.given_name}`}</Text>
                 <Text style={styles.fontSize}>{`조회수 ${item.view_count}`}</Text>
+                </View>
               </View>
             </View>
           </TouchableOpacity>
@@ -61,7 +64,6 @@ const PostList =({navigation})=>{
 
     return (
         <SafeAreaView>
-         <Text>될까요?</Text>
          {isLoding ? (
           <ActivityIndicator />
         ) : (
@@ -79,159 +81,34 @@ const PostList =({navigation})=>{
 }
 export default PostList;
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: "#fff",
-      alignItems: "center",
-      justifyContent: "center",
-    },
-    searchButton: {
-      right: 15,
-      left: 10,
-      backgroundColor: "#fff",
-      padding: 4,
-    },
-    keywordInput: {
-      left: 15,
-      width: 230,
-      backgroundColor: "#fff",
-      padding: 5,
-      margin: 0,
-    },
-    searchList: {
-      left: 15,
-      width: 120,
-      backgroundColor: "#900124",
-      padding: 5,
-      margin: 0,
-    },
     searchBoxList: {
-      left: 50,
-      width: 300,
-    },
-    listText: {
-      color: "#fff",
-      fontSize: 20,
-      left: 15,
-      alignContent: "center",
-    },
-    wrapper: {
-      flexDirection: "row",
-    },
-    wrapperCol: {
-      flexDirection: "column",
-    },
-  
-    item: {
-      backgroundColor: "#E6E5E6",
-      padding: 20,
-      borderColor: "#BFBFBF",
-      borderWidth: 0.8,
-    },
-    mainPostView: {
-      flex: 1,
-      width: "90%",
-      alignSelf: "center",
-    },
-    card: {
-      height: 100,
-      marginVertical: 10,
-      flexDirection: "row",
-      shadowColor: "#999",
-      shadowOffset: { width: 0, height: 1 },
-      shadowOpacity: 0.8,
-      shadowRadius: 2,
-      elevation: 5,
-    },
-    cardImgWrapper: {
-      flex: 1,
-    },
-    cardImg: {
-      height: "100%",
-      width: "100%",
-      alignSelf: "center",
-      borderRadius: 8,
-      borderBottomRightRadius: 0,
-      borderTopRightRadius: 0,
+      width: chartWidth,
     },
     cardInfo: {
-      flex: 2,
+      flex: 1,
       padding: 10,
       borderColor: "#ccc",
-      borderWidth: 1,
+      borderBottomWidth: 1,
       backgroundColor: "#fff",
     },
     cardTitle: {
       fontWeight: "bold",
     },
-    cardDetails: {
-      fontSize: 12,
-      color: "#444",
-    },
-    cardBottom1: {
-      flex: 3,
-      flexDirection: "row",
-    },
-    cardBottom2: {
-      flex: 1,
-      flexDirection: "row",
-      alignItems: "flex-end",
-    },
+    contentFont: {
+        fontSize: 12,
+        color: "black",
+      },
     fontSize: {
       fontSize: 8,
       color: "#444",
     },
-    container: {
-      flex: 1,
-      backgroundColor: "#fff",
-      alignItems: "center",
-      justifyContent: "center",
-    },
-    searchButton: {
-      right: 15,
-      left: 10,
-      backgroundColor: "#fff",
-      padding: 4,
-    },
-    contentSearch: {
-      left: 15,
-      width: 230,
-      backgroundColor: "#fff",
-      padding: 5,
-      margin: 0,
-    },
-    searchList: {
-      left: 15,
-      width: 120,
-      backgroundColor: "#900124",
-      padding: 5,
-      margin: 0,
-    },
-    searchBoxList: {
-      left: 50,
-      width: 300,
-    },
-    listText: {
-      color: "#fff",
-      fontSize: 20,
-      left: 15,
-      alignContent: "center",
-    },
-    wrapper: {
-      flexDirection: "row",
-    },
-    wrapperCol: {
-      flexDirection: "column",
-    },
-  
-    item: {
-      backgroundColor: "#E6E5E6",
-      padding: 20,
-      borderColor: "#BFBFBF",
-      borderWidth: 0.8,
-    },
     rowWrapper: {
       flexDirection: "row",
+      alignItems: 'center',
+      justifyContent: 'space-between',
     },
+    colWrapper:{
+        flexDirection: "column",
+    }
   });
   
