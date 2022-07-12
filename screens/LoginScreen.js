869952cc510dc3cf93d45ade3e6763897985a636
wakhoo <qqww212@korea.ac.kr>
2022-07-12 19:02:31 +0900

    no fetch

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -10,7 +10,7 @@ import * as Linking from 'expo-linking';
 
 WebBrowser.maybeCompleteAuthSession();
 
-export default function LoginScreen ({navigation}) {
+export default function LoginScreen ({navigation}){ 
  
   const [gUser, setGUser] = useState('');
   const [reqError, setReqError] = useState('');
@@ -56,46 +56,32 @@ export default function LoginScreen ({navigation}) {
             setReqError(error.response.data);
         }
         
-    }
-    
- const user=  { 
-    "accessToken": JSON.stringify(gAT),
-    "userInfo": {
-      "id": JSON.stringify(gUser.id),
-      "email": JSON.stringify(gUser.email),
-      "verified_email": JSON.stringify(gUser.verified_email),
-      "name": JSON.stringify(gUser.name),
-      "given_name": JSON.stringify(gUser.given_name),
-      "family_name": JSON.stringify(gUser.family_name),
-      "picture": JSON.stringify(gUser.picture),
-      "locale": JSON.stringify(gUser.locale),
-      "hd": JSON.stringify(gUser.hd)}}
-
-const giveGoogleUser = async () => {
-  let res = await fetch('https://library-2022.herokuapp.com/auth/google/user', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json;charset=utf-8'
-  },
-  body: JSON.stringify(user)
+      }
+// const giveGoogleUser = async () => {
+//   let res = await fetch('https://library-2022.herokuapp.com/auth/google/user', {
+//   method: 'POST',
+//   headers: {
+//     'Content-Type': 'application/json;charset=utf-8'
+//   },
+//   body: JSON.stringify(user)
   
-});
+// });
 
-let responed =await res.status;
-if(responed==201){
-  console.log(responed)
-  console.log("기존 회원입니다.")
-  storageData();
-  navigation.replace('Home');
-}else if(responed==200){
-  console.log(responed)
-  console.log("신규 회원입니다.")
-  storageData();
-  navigation.replace('Home');
-}else{
-  console.log(responed)
-}
-}
+// let responed =await res.status;
+// if(responed==201){
+//   console.log(responed)
+//   console.log("기존 회원입니다.")
+//   storageData();
+//   navigation.replace('Home');
+// }else if(responed==200){
+//   console.log(responed)
+//   console.log("신규 회원입니다.")
+//   storageData();
+//   navigation.replace('Home');
+// }else{
+//   console.log(responed)
+// }
+// }
 
 
 const giveGoogleUser = async (accessToken) => {
@@ -129,11 +115,11 @@ const giveGoogleUser = async (accessToken) => {
                 navigation.replace('Home');
               }
               
-//             } 
-//           )
-//         .catch(console.error)
-//         .finally(()=>setIsLoding(false));
-//     }
+            } 
+          )
+        .catch(console.error)
+        .finally(()=>setIsLoding(false));
+    }
 
     storageData=async()=>{
       await AsyncStorage.setItem(
