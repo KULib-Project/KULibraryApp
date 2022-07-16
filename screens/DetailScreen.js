a591daa166531fb521e74eef42cb5209b8a9e2be
VEA22 <76689078+VEA22@users.noreply.github.com>
2022-07-16 21:14:53 +0900

    Design detail screen
    #1. map 함수를 활용해 소장 정보 업데이트 완료
    #2. 레이아웃 일부 개편 적용

--- screens/DetailScreen.js
+++ screens/DetailScreen.js
@@ -1,21 +1,5 @@
-import React, { useEffect, useRef, useState } from "react";
-import {
-  View,
-  Text,
-  Image,
-  StyleSheet,
-  Dimensions,
-  StatusBar,
-  Platform,
-  Button,
-  TouchableOpacity,
-  FlatList,
-  ActivityIndicator,
-} from "react-native";
-import {
-  ImageHeaderScrollView,
-  TriggeringView,
-} from "react-native-image-header-scroll-view";
+import React, { useEffect, useState } from "react";
+import { View, Text, Image, StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 //책 정보를 보여주는 상자
@@ -24,6 +8,9 @@ const DetailScreen = ({ navigation, route }) => {
   const [data, setData] = useState([]);
   // 데이터를 저장하기 위한 변수 선언
   const [isLoding, setIsLoding] = useState(false);
+
+  let count = 0;
+  // 추후 소장 정보의 숫자를 나타낼 변수
 
   useEffect(() => {
     setIsLoding(true);
@@ -40,35 +27,37 @@ const DetailScreen = ({ navigation, route }) => {
     <SafeAreaView>
       <View>
         {/* 가장 큰 박스 */}
-        <View style={styles.columnWrapper}>
+        <View style={styles.imageWrapper}>
           {/* 이미지랑 정보 수평 배열 bookBox */}
           <Image
+            style={styles.imageStyle}
             source={{ uri: route.params.itemData.thumbnail }}
-            style={{ width: 150, height: 200 }}
           />
-          <View style={styles.rowWrapper}>
+          <View style={styles.detailWrapper}>
             {/* 정보 수직 배열 infoBox */}
-            <Text style={styles.cardInfo}>
+            <Text style={styles.inform}>
               {"제목: " + `${route.params.itemData.title}`}
-              {"\n" + "저자:  " + `${route.params.itemData.author}`}
+              {"\n\n" + "저자:  " + `${route.params.itemData.author}`}
             </Text>
           </View>
         </View>
-        <View style={styles.rowWrapper}>
+        <View style={styles.informWrapper}>
           {/* 대출정보 상자 stateBox -> api호출 변경하고 세부 정보 가져오는 코드 재확인 작업 할 것 
-                    가장 상층에 있는 정보들은 호출이 정상적으로 되는데 그 밑에 있는 정보들이 null로 표기됨 */}
-          <Text>대출 정보</Text>
+              가장 상층에 있는 정보들은 호출이 정상적으로 되는데 그 밑에 있는 정보들이 null로 표기됨 */}
+          <Text style={styles.cardTitle}>{"소장 정보"}</Text>
           <View style={styles.columnWrapper}>
             {/* 예약 정보 */}
-            {console.log("123123" + route.params?.itemData.collectInfo)}
             {route.params?.itemData.collectInfo.map((info) => {
               console.log(info.state);
-
+              count++;
               let a = (
-                <>
-                  <Text>{`${info.callNumber}`}</Text>
-                  <Text> {`${info.state}`}</Text>
-                </>
+                // Map을 통해 생성되는 각 객체에 key를 부여해야 오류가 발생하지 않음
+                // https://itprogramming119.tistory.com/entry/React-Warning-Each-child-in-a-list-should-have-a-unique-key-prop-%ED%95%B4%EA%B2%B0-%EB%B0%A9%EB%B2%95
+                <View key={info.callNumber} style={styles.ownerInform}>
+                  <Text style={styles.noTitle}>{"No. " + `${count}`}</Text>
+                  <Text>{"청구기호:  " + `${info.callNumber}`}</Text>
+                  <Text>{"도서상태: " + `${info.state}`}</Text>
+                </View>
               );
 
               return a;
@@ -82,24 +71,56 @@ const DetailScreen = ({ navigation, route }) => {
 
 export default DetailScreen;
 const styles = StyleSheet.create({
-  rowWrapper: {
+  backgroundWrapper: {
+    paddingLeft: 5,
+    paddingRight: 5,
+    backgroundColor: "#fff",
+  },
+  imageWrapper: {
+    width: "100%",
+    height: "60%",
     flexDirection: "row",
+    alignItems: "flex-start",
+    padding: 10,
+    backgroundColor: "#fff",
+  },
+  imageStyle: {
+    width: "45%",
+    height: "95%",
+  },
+  detailWrapper: {
+    width: "60%",
+    flexDirection: "row",
+    alignItems: "flex-end",
+  },
+  inform: {
+    alignItems: "flex-end",
+    padding: 10,
+    flexDirection: "row",
+    backgroundColor: "#fff",
+  },
+  informWrapper: {
+    flexDirection: "column",
   },
   columnWrapper: {
     flexDirection: "column",
-  },
-  cardInfo: {
-    flex: 3,
-    padding: 10,
-    borderColor: "#ccc",
-    borderWidth: 1,
+    justifyContent: "center",
     backgroundColor: "#fff",
+    padding: 10,
+  },
+  ownerInform: {
+    borderColor: "#eee",
+    borderBottomWidth: 0.5,
+    paddingTop: 2,
+    paddingBottom: 5,
   },
   cardTitle: {
     fontWeight: "bold",
+    fontSize: 20,
+    padding: 10,
+    backgroundColor: "#fff",
   },
-  searchBoxList: {
-    left: 50,
-    width: 300,
+  noTitle: {
+    fontWeight: "bold",
   },
 });
