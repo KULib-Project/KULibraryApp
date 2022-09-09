92831bd591ceeea67ea8de73484b8cb060d31ee3
전채원(Chaewon Jeon) <qqww212@korea.ac.kr>
2022-09-09 16:02:02 +0900

    Update PostDetail.js
    쿼리 시도..

--- screens/PostDetail.js
+++ screens/PostDetail.js
@@ -1,94 +1,93 @@
 import React, { useEffect, useRef, useState } from "react";
 import {
-  Text,
-  FlatList,
-  StyleSheet,
-  ActivityIndicator,
-  TouchableOpacity,
-  View,
-  Image,
-  Button,
-  TextInput
-} from 'react-native';
+	Text,
+	FlatList,
+	StyleSheet,
+	ActivityIndicator,
+	TouchableOpacity,
+	View,
+	Image,
+	Button,
+	TextInput
+} from "react-native";
 import {
-  ImageHeaderScrollView,
-  TriggeringView,
-} from 'react-native-image-header-scroll-view';
-import axios from 'axios';
+	ImageHeaderScrollView,
+	TriggeringView,
+} from "react-native-image-header-scroll-view";
+import axios from "axios";
 import async from "async";
 import { SafeAreaView } from "react-native-safe-area-context";
 import {
-  useQuery,
-  QueryClient,
-  QueryClientProvider,
-  useQueryClient
-} from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+	useQuery,
+	QueryClient,
+	QueryClientProvider,
+	useQueryClient
+} from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
-
 export default function PostDetail({ navigation,route }){
-  // API 쿼리 값 저장
-  const [comments, setComments] = useState([]);
-  /** 댓글 입력 값 저장 */
-  const [ccontent, setcontent] = useState(''); 
-  const [isLoding, setIsLoding] = useState(false);
+	// API 쿼리 값 저장
+	const [comments, setComments] = useState([]);
+	/** 댓글 입력 값 저장 */
+	const [ccontent, setcontent] = useState(""); 
+	const [isLoding, setIsLoding] = useState(false);
 
-  const getDetail = () => {
+	const getDetail = () => {
     
-    const id=route.params.itemData.id
-    return axios.get(`https://ea6717d8-601b-42a2-b0e6-1c5f0ea9f1a1.mock.pstmn.io/id=1`)
-    .then((response) => {
-      setComments(response.data)
-      console.log("==========================")
-      console.log(comments.comment.CComent.CcomentsList.map((c)=>(c.content)))
-      console.log("++++++++++++++++++++++++++")
+		const id=route.params.itemData.id;
+		return axios.get("https://ea6717d8-601b-42a2-b0e6-1c5f0ea9f1a1.mock.pstmn.io/id=1")
+			.then((response) => {
+				setComments(response.data);
+				console.log("==========================");
+				console.log(comments.comment.CComent.CcomentsList.map((c)=>(c.content)));
+				console.log("++++++++++++++++++++++++++");
 
       
-    })}
-    // const PrintComment = () => {
-    //   // useQuery를 활용한 Data fetch
-    //   const query = useQuery(['allcomment'], getDetail);
+			});};
+	const PrintComment = () => {
+	  // useQuery를 활용한 Data fetch
+	  const query = useQuery(['allcomment'], getDetail);
   
-    //   // Print Error
-    //   if (query.isError) {
-    //     console.log(query.error.message);
-    //   }
+	  // Print Error
+	  if (query.isError) {
+	    console.log(query.error.message);
+	  }
   
-    //   // Print Loading Screen
-    //   if (query.isLoading) {
-    //     return (
-    //       <View>
-    //         <Text>Loading...</Text>
-    //       </View>
-    //     );
-    //   } else {
-    //     // Complete loading
-    //     // 일반적인 상황에서는 출력이 잘 되나, 다른 스크린으로 이동했다가 재진입하면 undefined 에러가 발생, why?
-    //     return comments.comment.CComent.content.map((com) => (
-    //         // com.commentsList.map((c)=>(
-    //         //   <View>
-    //         //     <Text>ㅇ</Text>
-    //         //   </View>
+	  // Print Loading Screen
+	  if (query.isLoading) {
+	    return (
+	      <View>
+	        <Text>Loading...</Text>
+	      </View>
+	    );
+	  } else {
+	    // Complete loading
+	   
+	    return comments.comment.CComent.content.map((com) => (
+	        // com.commentsList.map((c)=>(
+	        //   <View>
+	        //     <Text>ㅇ</Text>
+	        //   </View>
   
-    //         // ))
-    //         <View>
-    //           <Text>{`${com}`}</Text>
-    //         </View>
-    //     ));
-    //   }
-    // };
+	        // ))
+	        <View>
+	          <Text>{`${com}`}</Text>
+	        </View>
+	    ));
+	  }
+	};
 
-    return(
-      <View>
-        {/* <QueryClientProvider client={queryClient}>
+	return(
+		<View>
+			<QueryClientProvider client={queryClient}>
           <PrintComment />
-        </QueryClientProvider> */}
-        {getDetail()}
-        <Text>전성기..^^</Text>
-      </View>
-    )
+        </QueryClientProvider>
+			{getDetail()}
+			<Text>전성기..^^</Text>
+		</View>
+	);
   
     
 }
